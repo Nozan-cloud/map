08a795d086f3bd7f3fda352b8d1dd60b638689e1
dazai <3546829078@qq.com>
2024-11-10 13:40:53 +0800

    map初始化

---
+++ pages/map/map.vue
@@ -0,0 +1,37 @@
+<template>
+	<view class="map-container">
+		<map style="width: 100%; height: 750rpx;" :latitude="latitude" :longitude="longitude" :markers="covers">
+		</map>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				id:0, // 使用 marker点击事件 需要填写id
+				title: 'map',
+				latitude: 23.1,
+				longitude: 113.26,
+				covers: [{
+					latitude: 23.1,
+					longitude: 113.26,
+					// iconPath: '../../../static/location.png'
+				}, {
+					latitude: 39.90,
+					longitude: 116.39,
+					// iconPath: '../../../static/location.png'
+				}]
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+.container-map{
+	width: 100%;
+	height: 100%;
+	
+}
+
+</style>
