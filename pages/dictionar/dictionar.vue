c1d0c6c896dccb7ed3eb550e6048e7ec2bb4064b
Kaysy <13825570890@163.com>
2024-11-10 22:14:39 +0800

    图鉴、图鉴详细信息

---
+++ pages/dictionar/dictionar.vue
@@ -0,0 +1,55 @@
+<template>
+  <view class="container">
+    <view class="image-list">
+      <view v-for="item in images" :key="item.id" class="image-item" @click="navigateToDetail(item)">
+       <image:src="item.src" class="img" mode="aspectFill"></image>
+        <text class="image-name">{{ item.name }}</text>
+      </view>
+    </view>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      images: [
+        { id: 1, name: '黄花风铃木', src: '/static/image/黄花风铃木.png' },
+        { id: 2, name: '紫荆', src: '/static/image/紫荆.png' },
+        { id: 3, name: '三角梅', src: '/static/image/三角梅.png' },
+        // 更多图片...
+      ]
+    };
+  },
+  methods: {
+    navigateToDetail(item) {
+      uni.navigateTo({
+        url: `/pages/photodetail/photodetail?name=${encodeURIComponent(item.name)}&url=${encodeURIComponent(item.url)}`
+      });
+    }
+  }
+};
+</script>
+
+<style>
+.container {
+  padding: 10px;
+}
+.image-list {
+  display: flex;
+  flex-wrap: wrap;
+}
+.image-item {
+  width: 30%;
+  margin: 5px;
+  text-align: center;
+}
+.img {
+  width: 100%;
+  height: auto;
+}
+.image-name {
+  margin-top: 5px;
+  font-size: 16px;
+}
+</style>
---
+++ pages/photodetail/photodetail.vue
@@ -0,0 +1,45 @@
+<template>  
+  <view class="detail-container">  
+    <image :src="imageUrl" class="detail-img" mode="aspectFit"></image>  
+    <text class="detail-name">{{ imageName }}</text>  
+    <button @click="goBack" class="back-button">返回</button>  
+  </view>  
+</template>  
+
+<script>  
+export default {  
+  data() {  
+    return {  
+      imageUrl: '',  
+      imageName: ''  
+    }  
+  },  
+  onLoad(options) {  
+    this.imageUrl = options.url;  
+    this.imageName = options.name;  
+  },  
+  methods: {  
+    goBack() {  
+      uni.navigateBack();  
+    }  
+  }  
+}  
+</script>  
+
+<style>  
+.detail-container {  
+  padding: 20px;  
+  text-align: center;  
+}  
+.detail-img {  
+  width: 100%;  
+  height: auto;  
+}  
+.detail-name {  
+  margin-top: 10px;  
+  font-size: 18px;  
+}  
+.back-button {  
+  margin-top: 20px;  
+}  
+</style>
